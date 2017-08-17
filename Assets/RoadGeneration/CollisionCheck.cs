18a6aebf5cd33e00713c1bcd267a4d6e99d471f1
Thomas Bech Madsen <thomasbm@live.dk>
2017-08-17 17:39:41 +0200

    Finished procedural road generator

---
+++ Assets/RoadGeneration/CollisionCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour {
+
+    RoadGenerator roadGenerator;
+
+    public void InitializeCollisionCheck(RoadGenerator roadGenerator)
+    {
+        this.roadGenerator = roadGenerator;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "CollisionChecker" && roadGenerator != null) {
+            roadGenerator.HandleCollision(transform.parent.gameObject, other.transform.parent.gameObject);
+        }
+    }
+}
--- Assets/RoadGenerator.cs
+++ Assets/RoadGenerator.cs
@@ -8,6 +8,7 @@ public class RoadGenerator : MonoBehaviour {
     public float deleteDistance; //Distance when the generator will start deleting pieces
 
     private List<GameObject> instantiatedPieces = new List<GameObject>();
+    private List<int> failures = new List<int>();
     private GameObject[] roadPieces;
 
 	// Use this for initialization
@@ -15,29 +16,95 @@ public class RoadGenerator : MonoBehaviour {
         roadPieces = Resources.LoadAll<GameObject>("RoadPieces");
         GameObject[] startPieces = Resources.LoadAll<GameObject>("StartPieces");
         instantiatedPieces.Add(Instantiate(startPieces[Random.Range(0, startPieces.Length - 1)]));
+        failures.Add(0);
+        //StartCoroutine(debugRoutine());
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        generatorPass();
+	}
+
+    void generatorPass()
+    {
         //Check delete distance on first instantiated piece
         if (Vector3.Distance(transform.position, instantiatedPieces[0].transform.position) > deleteDistance)
         {
             Destroy(instantiatedPieces[0]);
             instantiatedPieces.RemoveAt(0);
+            failures.RemoveAt(0);
         }
         //Check generation distance on the latest piece
-        if (Vector3.Distance(transform.position, instantiatedPieces[instantiatedPieces.Count-1].transform.position) < generationDistance)
+        if (Vector3.Distance(transform.position, instantiatedPieces[instantiatedPieces.Count - 1].transform.position) < generationDistance)
         {
             generatePiece();
         }
-	}
+    }
 
     void generatePiece()
     {
-        GameObject newPiece = Instantiate(roadPieces[Random.Range(0, roadPieces.Length - 1)]);
+        GameObject newPiece = Instantiate(roadPieces[Random.Range(0, roadPieces.Length)]);
         Transform lastPieceExit = instantiatedPieces[instantiatedPieces.Count - 1].transform.Find("ExitPoint");
         newPiece.transform.rotation = lastPieceExit.transform.rotation;
         newPiece.transform.position = lastPieceExit.position - newPiece.transform.Find("EntryPoint").position;
+        newPiece.GetComponentInChildren<CollisionCheck>().InitializeCollisionCheck(this);
         instantiatedPieces.Add(newPiece);
+        failures.Add(0);
+    }
+
+    public void HandleCollision(GameObject collider, GameObject collidedWith)
+    {
+        /*int colliderIndex = instantiatedPieces.IndexOf(collider);
+        int collidedWithIndex = instantiatedPieces.IndexOf(collidedWith);
+        if (collidedWithIndex >= 0 && colliderIndex > collidedWithIndex) {
+            print("Destroying " + colliderIndex + ", lost to " + instantiatedPieces.IndexOf(collidedWith));
+            instantiatedPieces.RemoveAt(colliderIndex);
+            Destroy(collider);
+
+            failureCount++;
+            if (failureCount == 3)
+            {
+                failureCount = 0;
+                Destroy(instantiatedPieces[colliderIndex - 1]);
+                instantiatedPieces.RemoveAt(colliderIndex - 1);
+            }
+        }*/
+
+        int colliderIndex = instantiatedPieces.IndexOf(collider);
+        int collidedWithIndex = instantiatedPieces.IndexOf(collidedWith);
+
+        if (collidedWithIndex >= 0 && colliderIndex > collidedWithIndex)
+        {
+            //print("Destroying " + colliderIndex + ", lost to " + instantiatedPieces.IndexOf(collidedWith));
+            instantiatedPieces.RemoveAt(colliderIndex);
+            failures.RemoveAt(colliderIndex);
+            Destroy(collider);
+
+            failures[colliderIndex - 1]++;
+            for (int i = colliderIndex - 1; i > 0; i--)
+            {
+                if (failures[i] >= 3)
+                {
+                    print("Destroying parent");
+                    failures[i - 1]++;
+                    Destroy(instantiatedPieces[i]);
+                    instantiatedPieces.RemoveAt(i);
+                    failures.RemoveAt(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    IEnumerator debugRoutine()
+    {
+        while (true)
+        {
+            generatorPass();
+            yield return new WaitForSeconds(1);
+        }
     }
 }
